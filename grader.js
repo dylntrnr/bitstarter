22f99457f70c9ca140cc2a1f63a675e86b56582e
Dylan Turner <dylan.turner22@gmail.com>
2013-07-08 22:49:43 +0000

    finished up grader.js

--- grader.js
+++ grader.js
@@ -25,8 +25,24 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = 'https://rawgithub.com/dylntrnr/bitstarter/master/index.html';
+
+var getUrl = function(url) {
+    rest.get(url).on('complete', function(result) {
+	saveResult(result);
+    });
+};
+
+var saveResult = function(urlFile) {
+    fs.writeFileSync('outfile.txt', urlFile);
+    var checkJson = checkHtmlFile('outfile.txt', program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+
+};
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -64,10 +80,15 @@ if(require.main == module) {
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url>', 'Url to url',  URL_DEFAULT)
 	.parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if(program.url) {
+	var url = getUrl(program.url);
+    } else {
+	var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
