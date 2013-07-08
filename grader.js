2ea06975963ce5f174e58d27882b459207b6fa28
Dylan Turner <dylan.turner22@gmail.com>
2013-07-08 04:53:48 +0000

    fixed .length

--- grader.js
+++ grader.js
@@ -50,7 +50,7 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-	var present = $(checks[ii]).lengh > 0;
+	var present = $(checks[ii]).length > 0;
 	out[checks[ii]] = present;
     }
     return out;
