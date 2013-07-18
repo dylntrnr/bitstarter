6809b30a5e6fa997c06a3b088a596893214a95db
Dylan Turner <dylan.turner22@gmail.com>
2013-07-18 06:38:01 -0600

    adding selfstarter html

--- web.js
+++ web.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var fs = require('fs');
+var path = require('path');
 
 var index = fs.readFileSync('index.html', 'utf8');
 var app = express.createServer(express.logger());
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(request, response) {
   response.send(index);
